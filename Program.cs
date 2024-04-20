29c271b45d5415d92603e0351a9add59fa865ee2
rohitdshinde <royshinde7744@gmail.com>
2024-04-20 10:53:59 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace FINALEXAM
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //string path = "E:\\anime\\FINALl.bin";
+
+            //string text = "helo mamam";
+            //File.WriteAllText(path, text);
+            //Console.WriteLine("Sucesss");
+
+            //string st=File.ReadAllText(path);
+            // Console.WriteLine(st);
+
+            // Console.ReadLine();
+
+            //using(BinaryWriter bi=new BinaryWriter(File.Open(path,FileMode.Create)))
+            // {
+            //     bi.Write(25);
+            //     bi.Write("Helo");
+            //     Console.WriteLine("Sucesss");
+            // }
+
+
+
+            //using(BinaryReader ba=new BinaryReader(File.Open(path,FileMode.Open)))
+            //{
+            //    int value=ba.ReadInt32();
+            //    string st=ba.ReadString();
+            //    Console.WriteLine(value+st);
+            //    Console.ReadLine();
+            //}
+
+
+
+            //DirectoryInfo doc= new DirectoryInfo(path);
+
+            //Console.WriteLine(doc.FullName);
+            //Console.ReadLine();
+
+
+            //FileInfo file= new FileInfo(path);
+
+            //Console.WriteLine(file.FullName);
+            //Console.ReadLine();
+
+
+
+
+            //XmlDocument doc = new XmlDocument();
+            // doc.Load("E:\\anime\\sem2\\FINALEXAM\\Myxml.xml");
+            //doc.Save(Console.Out);
+
+
+
+            //XmlNode node = doc.SelectSingleNode("//Name");
+            //Console.WriteLine(node.OuterXml);
+
+
+            //XmlElement emp = doc.CreateElement("Employee");
+
+            //XmlElement name=doc.CreateElement("Colour");
+            //name.InnerText = "Black";
+            //emp.AppendChild(name);
+
+            //XmlNode node = doc.SelectSingleNode("//Employee");
+            //node.AppendChild(name);
+
+            //doc.Save("E:\\anime\\sem2\\FINALEXAM\\Myxml.xml");
+
+            //Console.WriteLine("Sucess");
+
+            //Console.ReadLine();
+
+
+
+
+            //int[] age = { 1, 2, 47, 95 };
+
+            //var ab=from x in age select x;
+
+            //foreach (var xx in ab)
+            //{
+            //    Console.WriteLine(xx);
+               
+            //}
+
+
+            //List<int> list = new List<int> { 19,21,54,65}; 
+            //var age=from i in list where i<20 select i;
+
+            //foreach (int lii in age)
+            //{
+            //    Console.WriteLine(lii);
+            //}
+
+
+            //List<string> list = new List<string> { "Hii","li","zoro"};
+            //var ch = from c in list where c.Contains("i") select c;
+
+            //foreach(var ci in ch)
+            //{
+            //    Console.WriteLine(ci);
+            //}
+
+
+
+
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
